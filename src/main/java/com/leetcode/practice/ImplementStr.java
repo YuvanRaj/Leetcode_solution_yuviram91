cb77eba47f803587b531a02596f8b210dfbb9ea8
unknown <yuviram91@gmail.com>
2022-07-14 08:28:09 +0530

    Leetcode solution checkin

---
+++ src/main/java/com/leetcode/practice/ImplementStr.java
@@ -0,0 +1,13 @@
+package com.leetcode.practice;
+
+public class ImplementStr {
+
+	public static void main(String[] args) {
+		String haystack  = "aaaaa";
+		String needle   = "bba";
+		
+		System.out.println(haystack.indexOf(needle));
+
+	}
+
+}
